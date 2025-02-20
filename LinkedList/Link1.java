86d0dc3513b3b3cc8926395fda2a44ba76b2ad53
shubham singh <singhshubham68738@gmail.com>
2025-02-21 01:43:22 +0530

    working on linked list add addFirst And AddLast And then Print

---
+++ LinkedList/Link1.java
@@ -0,0 +1,54 @@
+public class Link1 {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void AddLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            tail = head = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Link1 l = new Link1();
+        l.addFirst(3);
+        l.addFirst(2);
+        l.addFirst(1);
+        l.AddLast(4);
+        l.AddLast(5);
+        l.print();
+
+    }
+}
